10898d921f6f6ce2937fb77c4434830c53154335
rutuja-nandiwadekar <nandiwadekarrutuja@gmail.com>
2022-07-01 00:23:33 +0530

    [Rutuja] Add : Code for Handling events

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,25 +1,37 @@
 import React from "react";
 
 function Form(props) {
-  return (
-    <form>
-      <h2 className="label-wrapper">
-        <label htmlFor="new-todo-input" className="label__lg">
-          What needs to be done?
-        </label>
-      </h2>
-      <input
-        type="text"
-        id="new-todo-input"
-        className="input input__lg"
-        name="text"
-        autoComplete="off"
-      />
-      <button type="submit" className="btn btn__primary btn__lg">
-        Add
-      </button>
-    </form>
-  );
+
+    const btn = document.querySelector('button');
+
+    btn.addEventListener('click', () => {
+        alert("hi!");
+    });
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        alert('Hello, world!');
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <h2 className="label-wrapper">
+                <label htmlFor="new-todo-input" className="label__lg">
+                    What needs to be done?
+                </label>
+            </h2>
+            <input
+                type="text"
+                id="new-todo-input"
+                className="input input__lg"
+                name="text"
+                autoComplete="off"
+            />
+            <button type="submit" className="btn btn__primary btn__lg" onClick={() => alert("hi!")}>
+                Add
+            </button>
+        </form>
+    );
 }
 
 export default Form;
