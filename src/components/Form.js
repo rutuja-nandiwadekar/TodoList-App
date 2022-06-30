a4c5f4194c3569c25384d8de2030e3df915fd6f5
rutuja-nandiwadekar <nandiwadekarrutuja@gmail.com>
2022-07-01 00:44:45 +0530

    [Rutuja] Add : Code to capture a user's input

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,49 +1,38 @@
-
 import React, { useState } from "react";
 
-
 function Form(props) {
-    const [name, setName] = useState('');
+  const [name, setName] = useState("");
 
+  function handleChange(e) {
+    setName(e.target.value);
+  }
 
-    // const btn = document.querySelector('button');
-
-    // btn.addEventListener('click', () => {
-    //     alert("hi!");
-    // });
-
-    function handleSubmit(e) {
-        e.preventDefault();
-        alert('Added successfully!');
-    }
-
-    function handleSubmit(e) {
-        e.preventDefault();
-        props.addTask("Say hello!");
-    }
-
-
-    return (
-        <form onSubmit={handleSubmit}>
-            <h2 className="label-wrapper">
-                <label htmlFor="new-todo-input" className="label__lg">
-                    What needs to be done?
-                </label>
-            </h2>
-            <input
-                type="text"
-                id="new-todo-input"
-                className="input input__lg"
-                name="text"
-                autoComplete="off"
-                value={name}
-            />
-
-            <button type="submit" className="btn btn__primary btn__lg" onClick={() => alert("hi!")}>
-                Add
-            </button>
-        </form>
-    );
+  function handleSubmit(e) {
+    e.preventDefault();
+    props.addTask(name);
+    setName("");
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2 className="label-wrapper">
+        <label htmlFor="new-todo-input" className="label__lg">
+          What needs to be done?
+        </label>
+      </h2>
+      <input
+        type="text"
+        id="new-todo-input"
+        className="input input__lg"
+        name="text"
+        autoComplete="off"
+        value={name}
+        onChange={handleChange}
+      />
+      <button type="submit" className="btn btn__primary btn__lg">
+        Add
+      </button>
+    </form>
+  );
 }
 
 export default Form;
